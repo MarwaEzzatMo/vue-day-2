4ec1c8c6d75527da00fcfe892a85fadc5ae685c6
MarwaEzzatMo <marwaezzat201@gmail.com>
2022-06-06 09:09:31 +0200

    day 2 tasks

---
+++ form-task/src/student.js
@@ -0,0 +1,7 @@
+export default [
+  { id: 1, name: "Marwa", city: "Sharqia" },
+  { id: 2, name: "Shaimaa", city: "Alex" },
+  { id: 3, name: "Omnia", city: "Cairo" },
+  { id: 4, name: "Nehal", city: "sharqia" },
+   { id: 5, name: "Nayra", city: "Monofia" },
+];
